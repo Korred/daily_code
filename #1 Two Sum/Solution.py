b1b3d2049a900958ba3f68591f9c11d7ed6131e0
Philipp Kochanski <P.Kochanski@uni-duesseldorf.de>
2017-10-22 22:15:01 +0200

    Solution to #1 Two Sum

---
+++ #1 Two Sum/Solution.py
@@ -0,0 +1,38 @@
+class Solution:
+
+    # Bruteforce approach
+    def twoSumBrute(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        for i, num_a in enumerate(nums):
+            for j, num_b in enumerate(nums[i+1:]):
+                if num_a + num_b == target:
+                    return [i, j+i+1]
+
+    # HashMap/dict() approach
+    def twoSumDict(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        checked = {}
+        for i, num in enumerate(nums):
+            if num in checked:
+                return [checked[num], i]
+
+            diff = target - num
+
+            if diff not in checked:
+                checked[diff] = i
+
+
+nums = [3, 2, 4, 12, 6, 125, 126, -4, -2]
+target = 123
+# Example use:
+print(Solution.twoSumDict(nums, target))
